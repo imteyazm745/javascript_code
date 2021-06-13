21790dc3bed41bb9de2465a24708878b7578f7d5
Md Imteyaz <52629525+imteyazm745@users.noreply.github.com>
2021-06-13 13:08:07 +0530

    Create object_method.js

---
+++ object_method.js
@@ -0,0 +1,11 @@
+const dictionary = {
+  car: 'automobile',
+  apple: 'healthy snack',
+  cat: 'cute furry animal',
+  dog: 'best friend'
+};
+Object.keys(dictionary);
+// ['car', 'apple', 'cat', 'dog']
+
+Object.values(dictionary);
+// ['automobile', 'healthy snack', 'cute furry animal', 'best friend']
